0c2d645d34c4c9e98542ffe8a4a93e6e1d799048
Brian Wilkins <ko4aqf@gmail.com>
2023-08-01 19:24:57 -0400

    Trying ljust

--- src/sduino/proto/morse2binary.py
+++ src/sduino/proto/morse2binary.py
@@ -26,11 +26,46 @@ chunkedList = wrap(spacebitsrus_bin, 8)
       
 # Iterate over each chunk in the chunked list
 for index, chunk in enumerate(chunkedList):      
-   # Append '00' to the chunk if its length is less than 7
-   if len(chunk) < 7:
-      chunk += "00"
+   if len(chunk) <= 7:
+      chunk += chunk.ljust(8-len(chunk),'0')
    if index != len(chunkedList) - 1:
       uintstr += "0b" + chunk + ","
    else:
       uintstr += "0b" + chunk + "};"
 print(uintstr)
+
+defcon31_morse = "-.. . ..-. -.-. --- -. ...-- .----"
+defcon31_bin = ""
+
+prevMorseChar = None
+for c in defcon31_morse:
+   if c == ".":
+      defcon31_bin += "10"
+      prevMorseChar = "dit"
+   elif c == "-":
+      defcon31_bin += "1110"
+      prevMorseChar = "dah"
+   else:
+      defcon31_bin += "0"
+      prevMorseChar = None
+
+print(defcon31_bin)
+print("DEFCON31")
+print(defcon31_morse)
+
+uintstr = "uint8_t morse_defcon[] = {"
+
+chunkedList = wrap(defcon31_bin, 8)
+      
+# Iterate over each chunk in the chunked list
+for index, chunk in enumerate(chunkedList):      
+   # Append '00' to the chunk if its length is less than 7
+   if len(chunk) <= 7:
+      chunk += chunk.ljust(8-len(chunk),'0')
+   if index != len(chunkedList) - 1:
+      uintstr += "0b" + chunk + ","
+   else:
+      uintstr += "0b" + chunk + "};"
+print(uintstr)
+
+
