922e56b7f5dba9675ef68ce72c70144b5f052171
Brian Wilkins <ko4aqf@gmail.com>
2023-08-01 19:11:56 -0400

    Now with ending curly brace

--- src/sduino/proto/morse2binary.py
+++ src/sduino/proto/morse2binary.py
@@ -25,10 +25,12 @@ uintstr = "uint8_t morse_space[] = {"
 chunkedList = wrap(spacebitsrus_bin, 8)
       
 # Iterate over each chunk in the chunked list
-for i, chunk in enumerate(chunkedList):      
+for index, chunk in enumerate(chunkedList):      
    # Append '00' to the chunk if its length is less than 7
    if len(chunk) < 7:
       chunk += "00"
-      
-   uintstr += "0b" + chunk + ","
+   if index != len(chunkedList) - 1:
+      uintstr += "0b" + chunk + ","
+   else:
+      uintstr += "0b" + chunk + "};"
 print(uintstr)
