afdf0b46b298c71578d1855ea500fab6c03aa575
Brian Wilkins <ko4aqf@gmail.com>
2023-08-01 20:28:30 -0400

    Completed morse 2 binary

--- src/sduino/proto/morse2binary.py
+++ src/sduino/proto/morse2binary.py
@@ -1,76 +1,62 @@
 from textwrap import wrap
 
-spacebitsrus_morse = "... .--. .- -.-. . -... .. - ... .-. ..- ..."
+def convert_to_binary(morse_str):
+    """
+    Convert a Morse code string to binary representation.
+    Args:
+        morse_str (str): The input Morse code string.
+    Returns:
+        str: The binary representation of the Morse code string.
+    """
+    binary_str = ""
+    for c in morse_str:
+        if c == ".":
+            binary_str += "10"
+        elif c == "-":
+            binary_str += "1110"
+        else:
+            binary_str += "0"
+    return binary_str
 
-spacebitsrus_bin = ""
+def create_uintstr(binary_str, variable_name):
+    """
+    Create a uint8_t array string from the binary string.
+    Args:
+        binary_str (str): The input binary string.
+        variable_name (str): The name of the uint8_t array variable.
+    Returns:
+        str: The uint8_t array string.
+    """
+    uintstr = f"uint8_t {variable_name}[] = {{"
+    chunkedList = wrap(binary_str, 8)
+    for index, chunk in enumerate(chunkedList):
+        # Pad the chunk with zeros to make it 8 bits long
+        chunk = chunk.ljust(8, '0')
+        if index != len(chunkedList) - 1:
+            uintstr += "0b" + chunk + ","
+        else:
+            uintstr += "0b" + chunk + "};"
+    return uintstr
 
-prevMorseChar = None
-for c in spacebitsrus_morse:
-   if c == ".":
-      spacebitsrus_bin += "10"
-      prevMorseChar = "dit"
-      print("dit","-")
-   elif c == "-":
-      spacebitsrus_bin += "1110"
-      prevMorseChar = "dah"
-      print("dah","-")
-   else:
-      spacebitsrus_bin += "0"
-      print(" ")
-      prevMorseChar = None
+def main():
+    # Input Morse code strings
+    spacebitsrus_morse = "... .--. .- -.-. . -... .. - ... .-. ..- ..."
+    defcon31_morse = "-.. . ..-. -.-. --- -. ...-- .----"
 
-print(spacebitsrus_bin)
-print("SPACEBITSRUS")
-print(spacebitsrus_morse)
+    # Convert Morse code to binary
+    spacebitsrus_bin = convert_to_binary(spacebitsrus_morse)
+    defcon31_bin = convert_to_binary(defcon31_morse)
 
-uintstr = "uint8_t morse_space[] = {"
+    # Create uint8_t array strings
+    uintstr_spacebitsrus = create_uintstr(spacebitsrus_bin, "morse_space")
+    uintstr_defcon31 = create_uintstr(defcon31_bin, "morse_defcon")
 
-chunkedList = wrap(spacebitsrus_bin, 8)
-      
-# Iterate over each chunk in the chunked list
-for index, chunk in enumerate(chunkedList): 
-   if len(chunk) < 7:
-      chunk += chunk.ljust(8-len(chunk),'0')
-   elif len(chunk) == 7:
-      chunk += "0"
-      
-   if index != len(chunkedList) - 1:
-      uintstr += "0b" + chunk + ","
-   else:
-      uintstr += "0b" + chunk + "};"
-print(uintstr)
+    # Print the result
+    print("Morse spacebitsrus:")
+    print(uintstr_spacebitsrus)
 
-defcon31_morse = "-.. . ..-. -.-. --- -. ...-- .----"
-defcon31_bin = ""
+    print("\nMorse defcon31:")
+    print(uintstr_defcon31)
 
-prevMorseChar = None
-for c in defcon31_morse:
-   if c == ".":
-      defcon31_bin += "10"
-      prevMorseChar = "dit"
-   elif c == "-":
-      defcon31_bin += "1110"
-      prevMorseChar = "dah"
-   else:
-      defcon31_bin += "0"
-      prevMorseChar = None
-
-print(defcon31_bin)
-print("DEFCON31")
-print(defcon31_morse)
-
-uintstr = "uint8_t morse_defcon[] = {"
-
-chunkedList = wrap(defcon31_bin, 8)
-      
-# Iterate over each chunk in the chunked list
-for index, chunk in enumerate(chunkedList): 
-   if len(chunk) <= 7:
-      chunk += chunk.ljust(8-len(chunk),'0')
-   if index != len(chunkedList) - 1:
-      uintstr += "0b" + chunk + ","
-   else:
-      uintstr += "0b" + chunk + "};"
-print(uintstr)
-
-
+if __name__ == "__main__":
+    main()
